56547cb251b3988a61c7383197ebfdadbe0e51a8
Erikson Kaszubowski <erikson.k@ufsc.br>
2022-12-16 15:11:06 -0300

    Implement todo item deleting function and user interface

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,12 @@
-import {
-    Priority,
-    TodoItem,
-    TodoList,
-    todoItemFactory,
-    toggleTodoItem,
-    changePriority,
-    changeDueTodoItem,
-} from "./modules/todo";
+import { projectFactory } from "./modules/project";
 
-import {
-    ChecklistItem,
-    Checklist,
-    checklistItemFactory,
-    toggleChecklistItem,
-    changeChecklistDate,
-} from "./modules/checklist";
-
-import { Note, NoteList, noteFactory } from "./modules/notes";
-
-import { Project, projectFactory } from "./modules/project";
-
-import {documentElements, buildItem, updateListDOM} from "./modules/todoDOM";
+import { documentElements, buildItem, updateListDOM } from "./modules/todoDOM";
 
 const defaultProject = projectFactory("Default");
 
 documentElements.button.addEventListener("click", () => {
-    if (!documentElements.titleInput.value) return
+    if (!documentElements.titleInput.value) return;
     const newItem = buildItem();
     defaultProject.todoList.push(newItem);
     updateListDOM(defaultProject);
-})
+});
--- src/modules/todoDOM.ts
+++ src/modules/todoDOM.ts
@@ -1,38 +1,54 @@
-import { Project } from "./project"
+import { Project } from "./project";
 import { TodoItem, todoItemFactory } from "./todo";
 
 const documentElements = {
     list: document.getElementById("list") as HTMLElement,
     titleInput: document.getElementById("title") as HTMLInputElement,
-    button: document.getElementById("add") as HTMLButtonElement
-}
+    button: document.getElementById("add") as HTMLButtonElement,
+};
 
 const buildItem = (): TodoItem => {
-    return todoItemFactory(documentElements.titleInput.value as string, documentElements.titleInput.value as string);
-}
+    return todoItemFactory(
+        documentElements.titleInput.value as string,
+        documentElements.titleInput.value as string
+    );
+};
 
-const removeItem = () => {console.log("removeItem")};
+const removeItem = (project: Project, e: MouseEvent): void => {
+    const originDiv: HTMLElement = e.composedPath()[1] as HTMLElement;
+    const idx: string | undefined = originDiv.dataset.index;
+    if (!idx) return;
+    project.todoList.splice(+idx, 1);
+};
 
-const generateItem = (todoItem: TodoItem): HTMLElement => {
+const generateItem = (
+    project: Project,
+    todoItem: TodoItem,
+    index: number
+): HTMLElement => {
     const container = document.createElement("div");
+    container.dataset.index = index.toString();
     const itemText = document.createElement("span");
     itemText.textContent = todoItem.title;
     container.appendChild(itemText);
 
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "✖";
-    deleteButton.addEventListener("click", removeItem);
+    deleteButton.addEventListener("click", (ev: MouseEvent): void => {
+        removeItem(project, ev);
+        updateListDOM(project);
+    });
     container.appendChild(deleteButton);
 
     return container;
-}
-
-const updateListDOM = (project: Project): void => {
-    documentElements.list.textContent = '';
-    project.todoList.map((val: TodoItem): void => {
-        const item = generateItem(val);
-        documentElements.list?.appendChild(item);
-    })
 };
 
-export {documentElements, buildItem, updateListDOM};
+const updateListDOM = (project: Project): void => {
+    documentElements.list.textContent = "";
+    project.todoList.forEach((val: TodoItem, idx: number): void => {
+        const item = generateItem(project, val, idx);
+        documentElements.list.appendChild(item);
+    });
+};
+
+export { documentElements, buildItem, updateListDOM };
