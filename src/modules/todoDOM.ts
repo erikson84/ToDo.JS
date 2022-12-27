ed6e76314348be61c32445717d96a6cd040b31b6
Erikson Kaszubowski <erikson84@yahoo.com.br>
2022-12-27 18:55:58 -0300

    Refactor generateItem as a pipeline of individual functions

--- src/modules/todoDOM.ts
+++ src/modules/todoDOM.ts
@@ -15,37 +15,53 @@ const documentElements = {
     button: document.getElementById("add") as HTMLButtonElement,
 };
 
-const buildItem = (): TodoItem => {
+const buildItemFromForm = (): TodoItem => {
     return todoItemFactory(
         documentElements.titleInput.value as string,
         documentElements.titleInput.value as string
     );
 };
 
-const generateItem = (
-    todoList: TodoList,
-    todoItem: TodoItem,
-    index: number
-): HTMLElement => {
+const createItem = (index: number): HTMLElement => {
     const container = document.createElement("div");
     container.classList.add("itemList");
     container.dataset.index = index.toString();
+    return container;
+};
 
+const addCheckbox = (
+    container: HTMLElement,
+    todoItem: TodoItem,
+    todoList: TodoList,
+    index: number
+): HTMLElement => {
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.checked = todoItem.done;
-    checkbox.name = "checkbox" + index;
     if (todoItem.done) container.classList.add("checkItem");
     checkbox.addEventListener("change", (ev: Event): void => {
         const stateList = applyItemFunction(index, toggleItem, todoList);
         controller(stateList);
     });
     container.appendChild(checkbox);
+    return container;
+};
 
+const addItemTitle = (
+    container: HTMLElement,
+    todoItem: TodoItem
+): HTMLElement => {
     const itemText = document.createElement("span");
     itemText.appendChild(document.createTextNode(todoItem.title));
     container.appendChild(itemText);
+    return container;
+};
 
+const addButton = (
+    container: HTMLElement,
+    todoList: TodoList,
+    index: number
+): HTMLElement => {
     const deleteButton = document.createElement("button");
     deleteButton.classList.add("deleteButton");
     deleteButton.textContent = "✖";
@@ -54,13 +70,29 @@ const generateItem = (
         controller(stateList);
     });
     container.appendChild(deleteButton);
-
     return container;
+};
+
+const pipe = (arg: HTMLElement, fns: Array<any>): HTMLElement => {
+    return fns.reduce((acc, fn) => fn(acc), arg);
+};
+
+const generateCompleteItem = (
+    todoList: TodoList,
+    todoItem: TodoItem,
+    index: number
+): HTMLElement => {
+    const container = createItem(index);
+    return pipe(container, [
+        (cont: HTMLElement) => addCheckbox(cont, todoItem, todoList, index),
+        (cont: HTMLElement) => addItemTitle(cont, todoItem),
+        (cont: HTMLElement) => addButton(cont, todoList, index),
+    ]);
 };
 
 const updateListDOM = (todoList: TodoList): Array<HTMLElement> => {
     const out = todoList.map((val: TodoItem, idx: number): HTMLElement => {
-        return generateItem(todoList, val, idx);
+        return generateCompleteItem(todoList, val, idx);
     });
     return out;
 };
@@ -72,7 +104,7 @@ const controller = (todoList: TodoList): void => {
     );
 
     const updateState = () => {
-        const newItem: TodoItem = buildItem();
+        const newItem: TodoItem = buildItemFromForm();
         const stateList: TodoList = addItem(newItem, todoList);
         documentElements.titleInput.value = "";
         controller(stateList);
@@ -88,4 +120,4 @@ const controller = (todoList: TodoList): void => {
     };
 };
 
-export { documentElements, buildItem, updateListDOM, controller };
+export { documentElements, buildItemFromForm, updateListDOM, controller };
